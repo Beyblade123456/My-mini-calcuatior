23c3609394e7de5300c191eaaf6d1b042e5d0570
speed <100178250+Beyblade123456@users.noreply.github.com>
2022-03-23 19:12:45 -0700

    Add files via upload

---
+++ C_29 Calucator.js
@@ -0,0 +1,16 @@
+function add(){
+    var result=Number(document.getElementById("fn").value)+Number(document.getElementById("sn").value);
+    document.getElementById("b1").value=result;
+};
+function subract(){
+    var result=Number(document.getElementById("fn").value)-Number(document.getElementById("sn").value);
+    document.getElementById("b2").value=result;
+};
+function multiplication(){
+    var result=Number(document.getElementById("fn").value)*Number(document.getElementById("sn").value);
+    document.getElementById("b3").value=result;
+};
+function division(){
+    var result=Number(document.getElementById("fn").value)/Number(document.getElementById("sn").value);
+    document.getElementById("b4").value=result;
+};
